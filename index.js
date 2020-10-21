62a032d6a7de95a16236dd147b00842d48a70e04
eebbesen <eebbesen.git@gmail.com>
2020-10-21 10:55:05 -0500

    tests passing

---
+++ index.js
@@ -0,0 +1,276 @@
+const Alexa = require('ask-sdk-core');
+const AWS = require('aws-sdk');
+const axios = require('axios')
+
+if (process.env.NODE_ENV == 'test') {
+  AWS.config.update({
+    region: 'local',
+    endpoint: 'http://localhost:8001'
+  });
+} else {
+  AWS.config.update({region: 'us-east-1'});
+}
+var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+
+const cityMappings = require("./artifacts/city_map.json");
+
+const usStates = [
+  'alabama',
+  'alaska',
+  'arizona',
+  'arkansas',
+  'california',
+  'colorado',
+  'connecticut',
+  'delaware',
+  'florida',
+  'georgia',
+  'hawaii',
+  'idaho',
+  'illinois',
+  'indiana',
+  'iowa',
+  'kansas',
+  'kentucky',
+  'louisiana',
+  'maine',
+  'maryland',
+  'massachusetts',
+  'michigan',
+  'minnesota',
+  'mississippi',
+  'missouri',
+  'montana',
+  'nebraska',
+  'nevada',
+  'new hampshire',
+  'new jersey',
+  'new mexico',
+  'new york',
+  'north carolina',
+  'north dakota',
+  'ohio',
+  'oklahoma',
+  'oregon',
+  'pennsylvania',
+  'rhode island',
+  'south carolina',
+  'south dakota',
+  'tennessee',
+  'texas',
+  'utah',
+  'vermont',
+  'virginia',
+  'washington',
+  'west virginia',
+  'wisconsin',
+  'wyoming'
+]
+
+const slotWorker = function(intent) {
+  let vals = { stateName: null, cityName: null }
+
+  if (typeof intent == "undefined" || typeof intent.slots == "undefined"){
+    return vals;
+  }
+
+  if (typeof intent.slots.stateName !== "undefined" && typeof intent.slots.stateName.value !== "undefined") {
+    vals.stateName = intent.slots.stateName.value.toLowerCase();
+  }
+
+  if (typeof intent.slots.cityName !== "undefined" && typeof intent.slots.cityName.value !== "undefined") {
+    vals.cityName = intent.slots.cityName.value.toLowerCase();
+  }
+
+  vals = cityExtractor(vals.cityName, vals.stateName);
+
+  console.log('SNOWMONITOR:location ********', vals.cityName + ' ********');
+
+  return vals;
+}
+
+// remove punctuation and whitespace from a location name
+const formatLocation = function(loc) {
+  return loc.replace(/\s/g, '').replace(/\W/g, '').toLowerCase();
+}
+
+const cityExtractor = function(cityName, stateName) {
+  if (typeof stateName !== 'undefined' && null != stateName){
+    for (var s in usStates) {
+      if (stateName.endsWith(usStates[s])) {
+        return {cityName: [cityName, stateName.split(usStates[s])[0]].join(" ").trim(), stateName: usStates[s]};
+      }
+    }
+  }
+
+  return {cityName: [cityName, stateName].join(' ').trim().toLowerCase(), stateName: null};
+}
+
+const logSelection = function(city, state) {
+  const params = {
+    TableName: 'snowmer_places',
+    Item: {
+      'city': city,
+      'state': state,
+      'date': new Date().toISOString()
+    }
+  };
+
+  docClient.put(params, function(err, data) {
+    if (err) {
+      console.log("Error writing to databse for " + city + " " + state, err);
+    } else {
+      console.log("Successfully logged " + city + " " + state, data);
+    }
+  });
+}
+
+const getCityStatus = function(city, state) {
+  logSelection(city, state);
+  const payload = cityMappings[formatLocation(city)];
+  if (typeof payload == 'undefined') {
+    return new Promise((r) => {return r('unknown')});
+  }
+
+  if (payload.yesCondition.length == 0 && payload.noCondition.length == 0) {
+    return new Promise((r) => {return r('maybe')});
+  }
+
+  return axios.get(payload.site)
+    .then(resp => {
+      for (let i = 0; i < payload.yesCondition.length; i++) {
+        if (resp.data.toLowerCase().includes(payload.yesCondition[i].toLowerCase())) {
+          return true;
+        }
+      }
+      return false;
+    });
+}
+
+// wraps non-amazon functionality
+const statusWorker = function(cityName, intentCityName) {
+  if (cityName) {
+    return getCityStatus(formatLocation(cityName))
+      .then(status => {
+        if ('unknown' == status) {
+          if (intentCityName && cityName == intentCityName.toLowerCase()) {
+            return 'need_city';
+          }
+          return "I don't have information about " + cityName + ", but I'll add it to my list of cities to learn about.";
+        } else if (status == true) {
+          return "A snow emergency is currently in effect for " + cityName;
+        } else if (status == 'maybe') {
+          let resp = cityName + " doesn't post snow emergency status on their website so I can't tell if there is one currently.";
+          resp += " " + cityName + "'s threshold for declaring a snow emergency is " + cityMappings[formatLocation(cityName)]['threshold'] + " inches.";
+          return resp
+        } else if (status == false) {
+          return "There is not a snow emergency in " + cityName + " right now";
+        } else {
+          return "I had an issue getting information for " + cityName;
+        }
+      })
+  } else {
+    return new Promise((r) => {return r('need_city')});
+  }
+}
+
+// from https://github.com/alexa/alexa-cookbook/blob/master/display-directive/bodyTemplate/index.js
+function supportsDisplay() {
+  const hasDisplay =
+    this.event &&
+    this.event.context &&
+    this.event.context.System &&
+    this.event.context.System.device &&
+    this.event.context.System.device.supportedInterfaces &&
+    this.event.context.System.device.supportedInterfaces.Display;
+
+  return hasDisplay;
+}
+
+const launchLambdaServiceWrapper = function(text, handlerInput) {
+  let w = 'For what city?';
+  if (typeof text !== undefined && null != text){
+    w = text + ". " + w;
+  }
+
+  return handlerInput.responseBuilder
+      .speak(w)
+      .reprompt('Which city?')
+      .getResponse();
+};
+
+const lambdaServiceWrapper = function(handlerInput) {
+  console.log('lambdaServiceWrapper.handlerInput.requestEnvelope');
+  console.dir(handlerInput.requestEnvelope, {depth: null});
+  const slots = slotWorker(handlerInput.requestEnvelope.request.intent);
+  if ('error' == slots) {
+    return handlerInput.responseBuilder
+      .speak('I am sorry, I am having problems right now.')
+      .getResponse();
+  }
+
+  return statusWorker(slots.cityName, handlerInput.requestEnvelope.request.intent.slots.cityName.value)
+    .then(resp => {
+      if ('need_city' == resp) {
+        launchLambdaServiceWrapper(null, handlerInput);
+      } else {
+        const payload = cityMappings[formatLocation(slots.cityName)];
+        return handlerInput.responseBuilder
+          .speak(resp);
+      }
+    });
+};
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    console.log('LaunchrequestEnvelope.handlerInput');
+    console.dir(handlerInput, {depth: null});
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    return lambdaServiceWrapper(handlerInput)
+  }
+};
+
+const LocationRequestHandler = {
+  canHandle(handlerInput) {
+    console.log('LocationrequestEnvelope.handlerInput');
+    console.dir(handlerInput, {depth: null});
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest';
+  },
+  handle(handlerInput) {
+    return lambdaServiceWrapper(handlerInput)
+  }
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
+
+let skill;
+
+exports.handler = Alexa.SkillBuilders.custom()
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    LocationRequestHandler)
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
+
+// for testing
+exports.slotWorker = slotWorker;
+exports.getCityStatus = getCityStatus;
+exports.formatLocation = formatLocation;
+exports.statusWorker = statusWorker;
+exports.lambdaServiceWrapper = lambdaServiceWrapper;
+exports.launchLambdaServiceWrapper = launchLambdaServiceWrapper;
+exports.cityExtractor = cityExtractor;
---
+++ test/index_test.js
@@ -0,0 +1,484 @@
+process.env.NODE_ENV = 'test'
+
+const chai = require('chai')
+const should = chai.should()
+const index = require('../index')
+const Alexa = require('ask-sdk-core')
+const axios = require('axios')
+const sandbox = require('sinon').createSandbox();
+const sinon = require('sinon');
+
+const LOCATION = {
+  "stateOrRegion": "MN",
+  "city": "saintpaul",
+  "countryCode": "US",
+  "postalCode": "55105",
+  "addressLine1": "1600 Grand Ave",
+  "addressLine2": null,
+  "addressLine3": null,
+  "districtOrCounty": null
+}
+
+var handlerInput = null;
+var responseBuilderMock = null;
+
+describe('skill methods', function(){
+  beforeEach(() => {
+    index.response = {
+      speak: function(text) {
+        console.log('SPEAK: ' + text);
+        this.text = text
+        return this;
+      },
+      listen: function(text) {
+        console.log('LISTEN: ' + text);
+        return this;
+      },
+      renderTemplate: function(template) {
+        console.log('RENDER TEMPLATE: ' + template);
+        this.template = template;
+        return this;
+      }
+    };
+
+    index.requestEnvelope = {
+      context: {
+        System: {
+          user: {
+            permissions: {
+              consentToken: 'c0nsentT0ken'
+            },
+            apiEndpoint: 'https://fake.aws.moc/api/v1',
+            device: {
+              deviceId: 'amzna8675309ona'
+            }
+          }
+        }
+      },
+      request: {
+        requestId: 77,
+        intent: {
+          slots: {
+            cityName: {
+              value: 'saint Paul'
+            }
+          }
+        }
+      }
+    };
+
+    const responseBuilder = {
+      speak: function(text) {
+        return this;
+      },
+      reprompt: function(text) {
+        return this;
+      },
+      getResponse: function() {
+        return 'done';
+      }
+    };
+
+    responseBuilderMock = sinon.mock(responseBuilder);
+
+    handlerInput = {
+      responseBuilder: responseBuilder,
+      requestEnvelope: {
+        context: {
+          System: {
+            user: {
+              permissions: {
+                consentToken: 'c0nsentT0ken'
+              },
+              apiEndpoint: 'https://fake.aws.moc/api/v1',
+              device: {
+                deviceId: 'amzna8675309ona'
+              }
+            }
+          }
+        },
+        request: {
+          requestId: 77,
+          intent: {
+            slots: {
+              cityName: {
+                value: 'saint Paul'
+              }
+            }
+          }
+        }
+      }
+    };
+  })
+
+  afterEach(() => {
+    sandbox.restore()
+  })
+
+  describe('statusWorker', function() {
+    it ('handles a non-city in the city slot', (done) => {
+      index.statusWorker('about', 'about')
+        .then(resp => {
+          resp.should.equal('need_city')
+
+          done()
+        })
+        .catch(err => {
+          done(err)
+        })
+    })
+
+    it ('handles a maybe city that notifies on website when there is an emergency', (done) => {
+      index.statusWorker('brooklyn park')
+        .then(resp => {
+          resp.should.equal("brooklyn park doesn't post snow emergency status on their website so I can't tell if there is one currently. brooklyn park's threshold for declaring a snow emergency is 2 inches.")
+
+          done()
+        })
+        .catch(err => {
+          done(err)
+        })
+    })
+
+    it ('handles a city that notifies on website when there is an emergency', (done) => {
+      const data = 'blahblahblah Saint Paul declares snow emergency stuff'
+      const resolved = new Promise((r) => r({ data }))
+      const axiosStub = sandbox.stub(axios, 'get').returns(resolved)
+
+      index.statusWorker('saint Paul')
+        .then(resp => {
+          resp.should.equal('A snow emergency is currently in effect for saint Paul')
+
+          done()
+        })
+        .catch(err => {
+          done(err)
+        })
+    })
+
+    it ('handles a city that notifies on website when there is no emergency', (done) => {
+      const data = 'blahblahblah there is currently no snow emergency in effect stuff'
+      const resolved = new Promise((r) => r({ data }))
+      const axiosStub = sandbox.stub(axios, 'get').returns(resolved)
+
+      index.statusWorker('saint Paul')
+        .then(resp => {
+          resp.should.equal('There is not a snow emergency in saint Paul right now')
+
+          done()
+        })
+        .catch(err => {
+          done(err)
+        })
+    })
+
+    it ('handles a city that does not notify on website', (done) => {
+      index.statusWorker('Rochester')
+        .then(resp => {
+          resp.should.equal("Rochester doesn't post snow emergency status on their website so I can't tell if there is one currently. Rochester's threshold for declaring a snow emergency is 2 inches.")
+
+          done()
+        })
+        .catch(err => {
+          done(err)
+        })
+    })
+
+    it ('handles a city that we do not track', (done) => {
+      index.statusWorker('Cleveland')
+        .then(resp => {
+          resp.should.equal("I don't have information about Cleveland, but I'll add it to my list of cities to learn about.")
+
+          done()
+        })
+        .catch(err => {
+          done(err)
+        })
+    })
+
+    it ('handles no city given', (done) => {
+      index.statusWorker()
+        .then(resp => {
+          resp.should.equal("need_city")
+
+          done()
+        })
+        .catch(err => {
+          done(err)
+        })
+    })
+  })
+
+  describe ('cityExtractor', function() {
+    it ('handles two-word cities', (done) => {
+      const resp = index.cityExtractor('brooklyn', 'center')
+      resp.cityName.should.equal('brooklyn center')
+
+      done()
+    })
+
+    it ('handles two-word cities with state', (done) => {
+      const resp = index.cityExtractor('brooklyn', 'center minnesota')
+
+      resp.cityName.should.eql('brooklyn center')
+
+      done()
+    })
+
+    it ('handles two-word cities with two-word state only first word flagged as city', (done) => {
+      const resp = index.cityExtractor('sioux', 'falls south dakota')
+      resp.cityName.should.eql('sioux falls')
+
+      done()
+    })
+
+    it ('handles three-word cities with one-word state only first word flagged as city', (done) => {
+      const resp = index.cityExtractor('theif', 'river falls wisconsin')
+      resp.cityName.should.eql('theif river falls')
+
+      done()
+    })
+  })
+
+  describe('getCityStatus', function() {
+    it ('returns unknown when it does not have the city', (done) => {
+      index.getCityStatus('paintsaul')
+        .then(resp => {
+          resp.should.equal('unknown')
+
+          done()
+        })
+        .catch(err => {
+          done(err)
+        })
+    })
+
+    it ('returns unknown when city has two words', (done) => {
+      index.getCityStatus('brooklyn center')
+        .then(resp => {
+          resp.should.equal('unknown')
+
+          done()
+        })
+        .catch(err => {
+          done(err)
+        })
+    })
+
+    it ('returns maybe when city does not post status', (done) => {
+      index.getCityStatus('rochester')
+        .then(resp => {
+          resp.should.equal('maybe')
+
+          done()
+        })
+        .catch(err => {
+          done(err)
+        })
+    })
+
+    describe('snow emergency in effect', () => {
+      it ('returns true', (done) => {
+        const data = 'blahblahblah Saint Paul declares Snow emergency stuff'
+        const resolved = new Promise((r) => r({ data }))
+        const axiosStub = sandbox.stub(axios, 'get').returns(resolved)
+
+        index.getCityStatus('SaintPaul')
+          .then((cityStatus) => {
+            cityStatus.should.equal(true)
+
+            done()
+          })
+          .catch(err => {
+            done(err)
+          })
+      })
+    })
+
+    describe('no snow emergency declared verified', function() {
+      beforeEach(() => {
+        const data = 'There is currently no SNow emergency in effect blahblahblah'
+        const resolved = new Promise((r) => r({ data }))
+        const axiosStub = sandbox.stub(axios, 'get').returns(resolved)
+      })
+
+      it ('returns false', (done) => {
+        index.getCityStatus('saintpaul')
+          .then((cityStatus) => {
+            cityStatus.should.equal(false)
+
+            done()
+          })
+          .catch(err => {
+            done(err)
+          })
+      })
+
+      it ('is case insensitive', (done) => {
+        index.getCityStatus('SAINtpaul')
+          .then((cityStatus) => {
+            cityStatus.should.equal(false)
+
+            done()
+          })
+          .catch(err => {
+            done(err)
+          })
+      })
+    })
+  })
+
+  describe('slot worker', () => {
+    it ('returns defaults with no intent', (done) => {
+      const resp = index.slotWorker(undefined)
+
+      resp.should.eql({ stateName: null, cityName: null })
+
+      done()
+    })
+
+    it ('returns defaults with no intent slots', (done) => {
+      const resp = index.slotWorker({fun_stuff: 'blah'})
+
+      resp.should.eql({ stateName: null, cityName: null })
+
+      done()
+    })
+
+    it ('returns hash with city slot', (done) => {
+      const resp = index.slotWorker({slots: {cityName: {value:'saint Paul'}}})
+      resp.should.eql({ stateName: null, cityName: 'saint paul' })
+
+      done()
+    })
+
+    it ('returns hash with state and city slot', (done) => {
+      const resp = index.slotWorker({slots: {cityName: {value:'saint Paul'}, stateName: {value: 'Minnesota'}}})
+
+      resp.should.eql({ stateName: 'minnesota', cityName: 'saint paul' })
+
+      done()
+    })
+  })
+
+  describe('formatLocation', () => {
+    it('strips whitespace', (done) => {
+      const loc = index.formatLocation('saint Paul')
+
+      loc.should.equal('saintpaul')
+
+      done()
+    })
+
+    it('strips punctuation and whitespace', (done) => {
+      const loc = index.formatLocation('St. Paul')
+
+      loc.should.equal('stpaul')
+
+      done()
+    })
+  })
+
+  describe('launchLambdaServiceWrapper', () => {
+    it ('works with text', (done) => {
+      index.launchLambdaServiceWrapper('do some help', handlerInput);
+      done()
+    })
+
+    it('works with null text', (done) => {
+      index.launchLambdaServiceWrapper(null, handlerInput);
+
+      done()
+    })
+
+    it('works with no text', (done) => {
+      index.launchLambdaServiceWrapper('', handlerInput);
+
+      done()
+    })
+  })
+
+  describe('lambdaServiceWrapper', () => {
+    it ('works', (done) => {
+      responseBuilderMock.expects('speak').once();
+
+      index.lambdaServiceWrapper(handlerInput);
+
+      done()
+    })
+
+    it ('binds to launchLambdaServiceWrapper', (done) => {
+      handlerInput.requestEnvelope.request.intent.slots.cityName.value = 'about'
+
+      index.lambdaServiceWrapper(handlerInput)
+        .then(resp => {
+          done()
+        })
+        .catch(err => {
+          done(err)
+        })
+    })
+
+    it ('handles city with policy', (done) => {
+      responseBuilderMock.expects('speak').once();
+
+      handlerInput.requestEnvelope.request.intent.slots.cityName.value = 'eagan'
+
+      index.lambdaServiceWrapper(handlerInput)
+        .then(resp => {
+          console.log('ababababa')
+          console.dir(resp, { depth: null })
+          console.log('ababababa')
+          console.dir(index.response, { depth: null })
+          // index.response.template.textContent.primaryText.text.should.equal("The City of Eagan has an odd/even winter parking schedule in place from November 15 – April 15. On even days: Parking is ALLOWED on the side of the street with EVEN house addresses. On odd days: Parking is ALLOWED on the side of the street with ODD house addresses.")
+
+          done()
+        })
+        .catch(err => {
+          done(err)
+        })
+    })
+
+    it ('handles two-word city with policy', (done) => {
+      handlerInput.requestEnvelope.request.intent.slots.cityName.value = 'saint cloud'
+
+      index.lambdaServiceWrapper(handlerInput)
+        .then(resp => {
+          // index.response.template.textContent.primaryText.text.should.equal("Generally, with a normal snowfall of 2 inches or more, the City plows all streets, alleys, parking lots, and affected sidewalks in the system. A normal call-out occurs at midnight or 1 a.m.")
+
+          done()
+        })
+        .catch(err => {
+          done(err)
+        })
+    })
+
+    it ('upper-cases city for display', (done) => {
+      handlerInput.requestEnvelope.request.intent.slots.cityName.value = 'saint cloud'
+
+      index.lambdaServiceWrapper(handlerInput)
+        .then(resp => {
+          // index.response.template.title.should.equal('Saint Cloud policy')
+
+          done()
+        })
+        .catch(err => {
+          done(err)
+        })
+    })
+  })
+
+  // used for live testing against websites, skipped for testing
+  describe('live test', function() {
+    it.skip ('returns data from the internet', (done) => {
+      index.getCityStatus('minneapolis')
+        .then( cityStatus => {
+
+          cityStatus.should.equal(false)
+
+          done()
+        })
+      })
+  })
+
+})
